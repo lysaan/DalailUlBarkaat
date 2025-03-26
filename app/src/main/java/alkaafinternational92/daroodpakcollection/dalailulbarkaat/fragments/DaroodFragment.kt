4efb9373737e533f99eac083ff2f142fd55233ee
Malik AB <malik4ab@gmail.com>
2025-03-26 09:09:22 +0500

    Count feature added for darood collection, Urdu font added.
    Took 22 minutes

--- app/src/main/java/alkaafinternational92/daroodpakcollection/dalailulbarkaat/fragments/DaroodFragment.kt
+++ app/src/main/java/alkaafinternational92/daroodpakcollection/dalailulbarkaat/fragments/DaroodFragment.kt
@@ -1,10 +1,5 @@
 package alkaafinternational92.daroodpakcollection.dalailulbarkaat.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import alkaafinternational92.daroodpakcollection.dalailulbarkaat.R
 import alkaafinternational92.daroodpakcollection.dalailulbarkaat.activities.MainActivity
 import alkaafinternational92.daroodpakcollection.dalailulbarkaat.others.MyEnum.Companion.TYPE_DAROOD_PAK_COLLECTION
@@ -12,12 +7,24 @@ import alkaafinternational92.daroodpakcollection.dalailulbarkaat.others.MyEnum.C
 import alkaafinternational92.daroodpakcollection.dalailulbarkaat.others.MyHelper
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.firestore.AggregateSource
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class DaroodFragment : Fragment() {
 
   private lateinit var myHelper: MyHelper
   private lateinit var context: Context
+  private val db = FirebaseFirestore.getInstance()
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
@@ -27,7 +34,7 @@ class DaroodFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    myHelper = MyHelper("BatteryFragment", requireContext())
+    myHelper = MyHelper("DaroodFragment", requireContext())
     context = requireContext()
 
     view.findViewById<LinearLayout>(R.id.darood_pak_collection).setOnClickListener {
@@ -42,6 +49,30 @@ class DaroodFragment : Fragment() {
       context.startActivity(intent)
     }
 
+    lifecycleScope.launch {
+      val count = countDarood("ar")
+      val count_warid_ul_ghaib = countDarood("warid_ul_ghaib")
+      myHelper.log("Total documents in 'ar': $count")
+      view.findViewById<TextView>(R.id.darood_pak_count).text = getString(R.string.darood_pak1, count)
+      view.findViewById<TextView>(R.id.warid_ul_ghaib_count).text = getString(R.string.warid_ul_ghaib_with_urdu1, count_warid_ul_ghaib)
+    }
+
   }
+
+  suspend fun countDarood(list_name: String): Long? {
+    return try {
+      val aggregateSnapshot = db?.collection("daroodarabic")
+        ?.document("Uzcijxle9p4leTfWJBxs")
+        ?.collection(list_name)
+        ?.count()
+        ?.get(AggregateSource.SERVER)
+        ?.await() // Use await() to wait for the result
+      aggregateSnapshot?.count
+    } catch (e: Exception) {
+      myHelper.log( "Error counting documents:$e")
+      null
+    }
+  }
+
 
 }
